0429b204ca63799261d80b35e862dfbb33e709ca
Tim Röthig <77003127+tim-roethig@users.noreply.github.com>
2026-04-30 14:34:50 +0200

    Create main.py

---
+++ api/main.py
@@ -0,0 +1,33 @@
+from fastapi.openapi.files import get_swagger_ui_html
+from fastapi.staticfiles import StaticFiles
+from fastapi import FastAPI, File, UploadFile
+
+from pipeline import DocProcessor
+from cache import Cache
+
+
+app = FastAPI()
+
+file_processor = DocProcessor()
+cache = Cache()
+
+@app.post("/file2vlm-content")
+async def upload_file(file: UploadFile = File(...)) -> list[dict]:
+    filename = file.filename
+    file_content = await file.read()
+    
+    hash_key = cache.hash_file(file_content=file_content)
+
+    vlm_content = cache.read_cache(hash_key)
+    if vlm_content:
+        return vlm_content
+
+    vlm_content = await file_processor.process(file_content=file_content, filename=filename)
+    cache.write_cache(hash_key=hash_key, vlm_content=vlm_content)
+
+    return vlm_content
+
+
+if __name__ == "__main__":
+    import uvicorn
+    uvicorn.run(app, host="localhost", port=8081)
